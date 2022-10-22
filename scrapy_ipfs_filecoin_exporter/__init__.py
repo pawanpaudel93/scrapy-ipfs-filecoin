eba9750fcdddc983ab40dedb7d62be45513019fa
Pawan Paudel <pawanpaudel93@gmail.com>
2022-10-23 01:05:40 +0545

    fixing issues related to settings

--- scrapy_ipfs_filecoin_exporter/__init__.py
+++ scrapy_ipfs_filecoin_exporter/__init__.py
@@ -1,3 +1,4 @@
 from .storage import W3SFeedStorage
+from .exporter import W3SExporter
 
 __version__ = '0.0.1'
--- scrapy_ipfs_filecoin_exporter/storage.py
+++ scrapy_ipfs_filecoin_exporter/storage.py
@@ -1,17 +1,18 @@
 from urllib.parse import urlparse
 
 from scrapy.extensions.feedexport import BlockingFeedStorage
+from scrapy.utils.project import get_project_settings
 
 
 class W3SFeedStorage(BlockingFeedStorage):
     def __init__(self, uri, *, feed_options=None):
-        from scrapy.crawler import settings
-        from .client import WSClient
+        settings = get_project_settings()
+        from .client import W3SClient
 
         u = urlparse(uri)
         self.file_name = u.path[1:]  # remove first "/"
-        api_key = settings.get["W3_API_KEY"]
-        self.ws_client = WSClient(api_key)
+        api_key = settings.get("W3_API_KEY")
+        self.ws_client = W3SClient(api_key)
 
     def _store_in_thread(self, file):
         file.seek(0)
